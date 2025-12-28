b8de853f5606cf6244556b8bffc6050416058ce5
praveenupskill190598-hue <timepass190598@gmail.com>
2025-12-28 14:32:37 +0530

    Add files via upload

---
+++ getStarted.js
@@ -0,0 +1,25 @@
+//"revision day 1"
+/* type of data type
+    1. Number
+    2. String
+    3. booean
+    4. udefined
+    5. null */
+
+/* Non- premitive data types */
+// numbers
+
+
+var names =1000   
+var phoneNumber ="praveen"
+var aboutNull =null
+testtry=100
+console.log(typeof phoneNumber, phoneNumber) //type of operator
+console.log(names)
+console.log(testtry)
+
+
+var bigValue=9923456789009876554567n //bigint
+console.log(typeof bigValue)
+console.log(typeof aboutNull)
+
